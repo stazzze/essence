bec832746e984fde6f95ebd3f7f0e0611d17e061
stazzze <153354406+stazzze@users.noreply.github.com>
2023-12-10 11:29:07 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,19 +3,38 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  // Replace 'stazzze/essence' with your GitHub username and repository name
-  const githubUrl = `https://raw.githubusercontent.com/stazzze/essence/main/index.html`;
+  try {
+    // Dynamically generate GitHub URL based on the request path
+    const path = request.url.pathname.slice(1);  // Remove leading "/"
+    const githubUrl = `https://raw.githubusercontent.com/stazzze/essence/main/${path}`;
 
-  // Fetch the content of index.html from GitHub
-  const response = await fetch(githubUrl);
+    // Fetch the content from GitHub
+    const response = await fetch(githubUrl);
 
-  // Modify the response headers if needed
-  const modifiedHeaders = new Headers(response.headers);
-  modifiedHeaders.set('Content-Type', 'text/html');
+    // Modify the response headers if needed
+    const contentType = getContentType(path);
+    const modifiedHeaders = new Headers(response.headers);
+    modifiedHeaders.set('Content-Type', contentType);
 
-  return new Response(await response.text(), {
-    status: response.status,
-    statusText: response.statusText,
-    headers: modifiedHeaders,
-  });
+    return new Response(await response.text(), {
+      status: response.status,
+      statusText: response.statusText,
+      headers: modifiedHeaders,
+    });
+  } catch (error) {
+    return new Response('Error fetching content', { status: 500 });
+  }
+}
+
+function getContentType(path) {
+  // Simple function to determine Content-Type based on file extension
+  if (path.endsWith('.html')) {
+    return 'text/html';
+  } else if (path.endsWith('.css')) {
+    return 'text/css';
+  } else if (path.endsWith('.js')) {
+    return 'application/javascript';
+  } else {
+    return 'text/plain';
+  }
 }
